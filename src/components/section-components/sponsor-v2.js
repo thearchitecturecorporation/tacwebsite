dc444b5354e6daa571c6120e980dd941f5b644a0
ismail-ismail123 <thearchitecturecorporation@gmail.com>
2024-07-24 12:34:05 +0530

    final changes

--- src/components/section-components/sponsor-v2.js
+++ src/components/section-components/sponsor-v2.js
@@ -19,8 +19,7 @@ class SponsorV2 extends Component {
 			        </div>
 			      </div> */}
 			      <div className="col-lg-2">
-			        <div className="ltn__brand-logo-item">
-			          {/* <img src={publicUrl+"assets/img/Allimgs/logo2.png"} alt="Brand Logo" /> */}
+				  <div className="ltn__brand-logo-item">
 			          <img src={publicUrl+"assets/img/Allimgs/hasmai.png"} alt="Brand Logo" />
 			        </div>
 			      </div>
